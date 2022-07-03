6bbb7adb08443054272a767db1b073632a84ec08
Sushrut_Sharingan <sushrutmishra27@gmail.com>
2022-07-03 22:35:53 +0530

    Card update

--- force-app/main/default/lwc/cycleCard/cycleCard.js
+++ force-app/main/default/lwc/cycleCard/cycleCard.js
@@ -1,9 +1,9 @@
-// app.js
 import { LightningElement } from 'lwc';
-
 export default class CycleCard extends LightningElement {
-    bike = {
-        name: 'Electra X4',
-        picture: 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg'
-    };
-}
+   name = 'Electra X4';
+   description = 'A sweet bike built for comfort.';
+   category = 'Mountain';
+   material = 'Steel';
+   price = '$2,700';
+   pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
+ }
